f0bf74c0f4dc95ba638d921d7dda63acabad83cc
Ruslan Gonzalez <ruslanguns@gmail.com>
2022-04-23 17:27:59 +0100

    Home lifecycle

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,60 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
-  template: `Hola desde home!`,
+  template: `
+    Hola desde <app-onchanges [nombre]="nombre"></app-onchanges>!
+
+    <button
+      class="p-5 bg-green-700 rounded-lg text-white block m-2"
+      (click)="cambiarNombre()"
+    >
+      Cambiar nombre
+    </button>
+  `,
 })
-export class HomeComponent {}
+export class HomeComponent
+  implements OnInit, AfterContentInit, AfterViewInit, OnDestroy
+{
+  nombre = 'HomeComponent';
+  interval$: any;
+  count = 0;
+
+  constructor() {
+    console.log('HomeComponent constructor');
+    this.interval$ = setInterval(() => {
+      this.count++;
+      console.log('HomeComponent constructor interval', this.count);
+    }, 10000);
+  }
+
+  ngOnInit(): void {
+    console.log('HomeComponent ngOnInit');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('HomeComponent ngAfterContentInit');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('HomeComponent ngAfterViewInit');
+  }
+
+  ngOnDestroy(): void {
+    console.log('HomeComponent ngOnDestroy');
+
+    clearInterval(this.interval$);
+  }
+
+  cambiarNombre() {
+    console.log('HomeComponent cambiarNombre()');
+    this.nombre = 'Nuevo nombre';
+  }
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { NameComponent } from './home/onchanges.component';
 import { PagesRoutingModule } from './pages-routing.module';
 
 @NgModule({
   imports: [SharedModule, PagesRoutingModule],
-  declarations: [HomeComponent, AboutComponent],
+  declarations: [HomeComponent, NameComponent, AboutComponent],
   exports: [],
   providers: [],
 })
