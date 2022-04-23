6a06ebdb6ad05b97bbb765b04b9c38381a6f18f4
Ruslan Gonzalez <ruslanguns@gmail.com>
2022-04-23 16:20:48 +0100

    Feature: Logo Image Component

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,3 +1,4 @@
+import { LogoImageComponent } from './logo-image/logo-image.component';
 import { LogoComponent } from './logo/logo.component';
 
-export const SharedComponents = [LogoComponent];
+export const SharedComponents = [LogoComponent, LogoImageComponent];
--- src/app/shared/components/logo/logo.component.ts
+++ src/app/shared/components/logo/logo.component.ts
@@ -6,24 +6,7 @@ import { Component, Input } from '@angular/core';
     <a
       class="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="w-10 h-10 p-2 text-white bg-red-600 rounded-full"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path d="M12 14l9-5-9-5-9 5 9 5z" />
-        <path
-          d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-        />
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-        />
-      </svg>
+      <app-logo-image></app-logo-image>
       <span class="ml-3 text-xl">{{ appName }}</span>
     </a>
   `,
